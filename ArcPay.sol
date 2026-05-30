1103cae5892d258bbbfaef1f6c6bab1d5032c310
Md Hasib <103415835+Hasibuj@users.noreply.github.com>
2026-05-30 11:17:54 +0600

    Update ArcPay.sol

--- ArcPay.sol
+++ ArcPay.sol
@@ -1,21 +1,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+// Using official GitHub raw paths to avoid dependency resolution errors
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/ReentrancyGuard.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
 
-contract ArcPay is ReentrancyGuard, Ownable(msg.sender) {
+contract ArcPay is ReentrancyGuard, Ownable {
     
     event PaymentReceived(address indexed sender, uint256 amount);
     event FundsWithdrawn(address indexed owner, uint256 amount);
 
-    // Function to receive payments
+    constructor() Ownable(msg.sender) {}
+
     function pay() external payable nonReentrant {
         require(msg.value > 0, "Amount must be greater than 0");
         emit PaymentReceived(msg.sender, msg.value);
     }
 
-    // Function to withdraw funds
     function withdraw() external onlyOwner nonReentrant {
         uint256 balance = address(this).balance;
         require(balance > 0, "No funds to withdraw");
